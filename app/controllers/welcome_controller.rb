10d4465c03898413a91335e90af14082c6696a93
NestlyS <dronmc19@gmail.com>
2019-08-08 02:48:03 +0300

    update

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,15 +1,10 @@
 class WelcomeController < ApplicationController
   def index
       @projects = Project.all
-      @todo = Todo.new
   end
   def new
 
   end
   def update
     end
-    private
-        def todo_params
-            params.require(:edit_todo_).permit(:isChecked)
-        end
 end
