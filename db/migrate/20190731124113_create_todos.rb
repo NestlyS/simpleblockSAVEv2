ce7b34a9e0d493cf9997e640f7835e58315eb451
NestlyS <dronmc19@gmail.com>
2019-08-08 03:17:52 +0300

    Update 20190731124113_create_todos.rb

--- db/migrate/20190731124113_create_todos.rb
+++ db/migrate/20190731124113_create_todos.rb
@@ -2,7 +2,7 @@ class CreateTodos < ActiveRecord::Migration[5.1]
   def change
     create_table :todos do |t|
       t.text :text
-      t.boolean :isCompleted, 
+      t.boolean :isCompleted 
       t.references :project, foreign_key: true
 
       t.timestamps
